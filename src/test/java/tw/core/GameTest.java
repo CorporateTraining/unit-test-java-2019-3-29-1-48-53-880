fc2466a33d89b5cb4080c28171453c2a4dcd73e2
mahuanfy <mahuanfy@foxmail.com>
2019-04-19 13:46:16 +0800

    Add input validator test

--- src/test/java/tw/core/GameTest.java
+++ src/test/java/tw/core/GameTest.java
@@ -1,6 +1,12 @@
 package tw.core;
 
+import org.junit.Assert;
 import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.generator.AnswerGenerator;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 在GameTest文件中完成Game中对应的单元测试
@@ -10,18 +16,27 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void guess() {
+    public void guess_test() throws OutOfRangeAnswerException {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        Game game = new Game(answerGenerator);
+        Assert.assertEquals(game.guess(Answer.createAnswer("1 2 4 3")).getResult(),"2A2B");
     }
 
     @Test
-    public void guessHistory() {
+    public void guess_test_two() throws OutOfRangeAnswerException {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        Game game = new Game(answerGenerator);
+        Assert.assertEquals(game.guess(Answer.createAnswer("5 6 7 8")).getResult(),"0A0B");
     }
 
     @Test
-    public void checkCoutinue() {
-    }
+    public void check_status_test() throws OutOfRangeAnswerException {
+        AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+        when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
+        Game game = new Game(answerGenerator);
 
-    @Test
-    public void checkStatus() {
+        Assert.assertEquals(game.checkStatus(), "continue");
     }
 }
---
+++ src/test/java/tw/validator/InputValidatorTest.java
@@ -0,0 +1,12 @@
+package tw.validator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InputValidatorTest {
+
+    @Test
+    public void validate_test() {
+    }
+}
