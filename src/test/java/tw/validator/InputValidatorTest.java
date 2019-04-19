33c3810dad153e8bcc9e757c2b084df38b1761dc
mahuanfy <mahuanfy@foxmail.com>
2019-04-19 13:52:09 +0800

    Finish input validator test

--- src/test/java/tw/validator/InputValidatorTest.java
+++ src/test/java/tw/validator/InputValidatorTest.java
@@ -1,5 +1,6 @@
 package tw.validator;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,6 +8,20 @@ import static org.junit.Assert.*;
 public class InputValidatorTest {
 
     @Test
-    public void validate_test() {
+    public void validate_success_test() {
+        InputValidator inputValidator = new InputValidator();
+        Assert.assertEquals(inputValidator.validate("1 3 4 5"), true);
+    }
+
+    @Test
+    public void validate_fail_test() {
+        InputValidator inputValidator = new InputValidator();
+        Assert.assertEquals(inputValidator.validate("1 3 3 5"), false);
+    }
+
+    @Test
+    public void validate_fail_test_two() {
+        InputValidator inputValidator = new InputValidator();
+        Assert.assertEquals(inputValidator.validate("1 33 5"), false);
     }
 }
